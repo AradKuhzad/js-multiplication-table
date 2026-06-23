f0cc4867b1c60a42775755ca8211667081f54328
Arad <aradkuhzad@gmail.com>
2026-06-23 22:14:30 +0330

    Refactor script.js to remove console logs, streamlining the table generation function for improved clarity and performance.

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function generatebtn() {
     document.getElementById("result-table").innerHTML = "";
-    console.log(document.getElementById("rows-input").value);
-    console.log(document.getElementById("columns-input").value);
-    console.log("it work");
     let rows = document.getElementById("rows-input").value;
     let columns = document.getElementById("columns-input").value;
     let table = document.getElementById("result-table");
