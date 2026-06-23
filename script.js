caa01718252f8ea4bb83d202be3c0b9b75931665
Arad <aradkuhzad@gmail.com>
2026-06-23 18:27:46 +0330

    Refactor generatebtn function in script.js to log column input and implement nested loops for creating table cells based on user input for rows and columns

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function generatebtn() {
     document.getElementById("result-table").innerHTML = "";
     console.log(document.getElementById("rows-input").value);
+    console.log(document.getElementById("columns-input").value);
+    console.log("it work");
 }
